191cf952332bd04f5d57e172cc2dee32f92e7c10
mariannebourcier <marianne.bourcier@gmail.com>
2022-04-20 12:14:25 -0600

    solution created for wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
 
-module.exports = wordSearch
+  let verticalMatrix = [];
+  for (let i = 0; i < letters[0].length; i++) {
+    let array = [];
+    for (let y = 0; y < letters.length; y++) {
+      array.push(letters[y][i]);
+    }
+    verticalMatrix.push(array);
+  }
+
+  const verticalJoin = verticalMatrix.map(ls => ls.join(''));
+  for (let x of verticalJoin) {
+    if (x.includes(word)) {
+      return true;
+    }
+  }
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+//transpose, map, join, includes
+
+module.exports = wordSearch;
